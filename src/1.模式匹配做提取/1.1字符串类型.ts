5ba400f338778058b98fbf9134a47bac5002ca8d
lizq <lizq@epshealth.com>
2022-11-16 18:31:02 +0800

    feat: 字符串类型替换

--- src/1.模式匹配做提取/1.1字符串类型.ts
+++ src/1.模式匹配做提取/1.1字符串类型.ts
@@ -8,3 +8,14 @@ type StartWith<
 
 // type StartWithResult = StartWith<"sy", "s">; // true
 type StartWithResult = StartWith<"sy", "y">; // true
+
+// 2. 实现字符串替换
+type ReplaceStr<
+  Str extends string,
+  From extends string,
+  To extends string
+> = Str extends `${infer Prefix}${From}${infer Suffix}`
+  ? `${Prefix}${To}${Suffix}`
+  : never;
+
+type ReplaceStrResult = ReplaceStr<"zhangsan", "san", "ba">; // 'zhangba'
