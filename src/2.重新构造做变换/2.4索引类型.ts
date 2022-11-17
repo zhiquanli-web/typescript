9c0efcd9c60bc8add2cc321b6a046ef42cd8f764
lizq <lizq@epshealth.com>
2022-11-17 09:34:38 +0800

    feat: 重新构造总结

--- src/2.重新构造做变换/2.4索引类型.ts
+++ src/2.重新构造做变换/2.4索引类型.ts
@@ -20,3 +20,38 @@ type UppercaseKey<Obj extends object> = {
 };
 
 type UppercaseKeyResult = UppercaseKey<{ abc: 2 }>; // { ABC: 2; }
+
+// 4. TypeScript 提供了内置的高级类型 Record 来创建索引类型：
+// type Record<K extends string | number | symbol, T> = { [P in K]: T };
+type RecordResult = Record<"a", 1>; // { a: 1; }
+
+// 5. 给索引类型添加readonly
+type ToReadonly<T> = {
+  readonly [Key in keyof T]: T[Key];
+};
+
+type ToReadonlyResult = ToReadonly<{ a: 1 }>;
+
+// 5.1 移除readonly
+type ToMutable<T> = {
+  -readonly [Key in keyof T]: T[Key];
+};
+
+// 6. 添加可选修饰符
+type ToPartial<T> = {
+  [Key in keyof T]?: T[Key];
+};
+
+type ToPartialResult = ToPartial<{ a: 2 }>;
+
+// 6.1 移除可选修饰符
+type ToRequired<T> = {
+  [Key in keyof T]-?: T[Key];
+};
+
+// 7. 根据值过滤
+type FilterByValueType<Obj extends Record<string, any>, ValueType> = {
+  [Key in keyof Obj as Obj[Key] extends ValueType ? Key : never]: Obj[Key];
+};
+
+type FilterByValueTypeResult = FilterByValueType<{ a: 123; b: "str" }, "str">;
